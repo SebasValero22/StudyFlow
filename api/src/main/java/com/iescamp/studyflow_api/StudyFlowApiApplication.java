81fb3efd3606523453bda130f598d60ca23ce988
sebas <juavalmar2@alu.edu.gva.es>
2026-02-27 18:09:31 +0100

    Fix syntax error in StudyFlowApiApplication.java

--- api/src/main/java/com/iescamp/studyflow_api/StudyFlowApiApplication.java
+++ api/src/main/java/com/iescamp/studyflow_api/StudyFlowApiApplication.java
@@ -1,11 +1,10 @@
-3package com.iescamp.studyflow_api;
+package com.iescamp.studyflow_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
-StudyFlowApiApplication {
+public class StudyFlowApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudyFlowApiApplication.class, args);
